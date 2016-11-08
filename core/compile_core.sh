de985045588896037a6e55cad4826bfd2ebf3c61
Jeongbin Park <pjb7687@gmail.com>
2016-11-08 19:23:31 +0900

    Add needed packages' repo

--- core/compile_core.sh
+++ core/compile_core.sh
@@ -1,4 +1,6 @@
 cp jbfilereader/jbfilereader.coffee .
+cp pako/dist/pako_inflate.min.js .
+cp needle_js/needle.js .
 coffee --bare --compile jbfilereader.coffee
 coffee --bare --compile fastq-join.coffee
 coffee --bare --compile miseq-analyzer.coffee
